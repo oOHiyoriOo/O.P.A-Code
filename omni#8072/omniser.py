ae45e220db4e0ea5243be663bd517a6f6ff995a5
foktom <35112981+foktom@users.noreply.github.com>
2020-02-21 20:17:26 +0100

    updated serial script, now accepting both servos, database inputs done

--- omni#8072/omniser.py
+++ omni#8072/omniser.py
@@ -3,78 +3,95 @@ import os, sys
 from tinydb import TinyDB, Query
 
 
-
 query = Query()
 
 if not os.path.isdir("db"):
     os.system("mkdir db")
 
+try:
+    open("db/curservos.json", "w").close() #TODO: this is nonsense, make better workaround, not now tho
+    sdb = TinyDB("db/curservos.json")
+except:
+    print("restart script as sudo.")
+    sys.exit()
 
-open("db/curservos.json", "w").close() #TODO: this is nonsense, make better workaround, not now tho
-sdb = TinyDB("db/curservos.json")
 
-data = {}
-data["id"] = 0
-data["pos"] = 0
+PORT = ""
+if os.name == "nt":
+    PORT = "COM8"
+else:
+    PORT = "/dev/ttyACM0"
 
-sdb.insert(data)
+#For windows: "/COMX"  RPI: "/dev/ttyACM0"
+
 
 def clear(): 
     if os.name == 'nt': 
         _ = os.system('cls') 
     else: 
         _ = os.system('clear') 
-#For windows: "/COMX"  RPI: "/dev/ttyACM0"
-ser = serial.Serial("/dev/ttyACM0", 9600)
 
-print("connected.\n")
+
+ser = serial.Serial(PORT, 9600)
+
+
+#reset position and db on start
+data = "0.set0".encode()
+ser.write(data)
+data = "1.set0".encode()
+ser.write(data)
+
+data = {}
+data["id"] = 0
+data["pos"] = 0
+sdb.insert(data)
+
+data = {}
+data["id"] = 1
+data["pos"] = 0
+sdb.insert(data)
+
+
+print("Ready.\n")
 
 def send(data:str):
     OUT = data.replace("> ", "")
 
-    if OUT == "r45":
-        OUT = "r150"
-    elif OUT == "r90":
-        OUT = "r240"
-    elif OUT == "l45":
-        OUT = "l150"
-    elif OUT == "l90":
-        OUT = "l240"
-
-    elif OUT == "terminate":
-        print("terminating. . .")
-        curpos = int(sdb.search(query.id == 0)[0]['pos'])
-
-        if curpos < 0:
-            OUT = "r" +  str(curpos).replace("-", "")
-            print(OUT)
-        elif curpos > 0:
-            OUT = "l" + str(curpos)
-
+    #end command
+    if OUT == "end":
+        print("terminating . . .")
+        OUT = "0.set0"
         data = OUT.encode()
         ser.write(data)
-        print("Terminated.")
+        OUT = "1.set0"
+        data = OUT.encode()
+        ser.write(data)
         sys.exit()
-
-    curpos = int(sdb.search(query.id == 0)[0]['pos'])
-
-    if "l" in OUT:
-        outdata = OUT.replace("l", "")
-        newpos = int(curpos) - int(outdata)
-
-    elif "r" in OUT:
-        outdata = OUT.replace("r", "")
-        newpos = int(curpos) + int(outdata)
-
 
     data = OUT.encode()
     print("sending " + str(OUT))
 
+    #add new pos to db
+    if "0." in OUT:
+        if "end" in OUT:
+            newpos = "0"
+        else:
+            outdata = OUT.replace("0.set", "")
+            newpos = str(outdata)
+        
+        sdb.update({"pos":str(newpos)}, query.id == 0)
 
-    #print(curpos)
+    elif "1." in OUT:
+        if "end" in OUT:
+            newpos = "0"
+        else:
+            outdata = OUT.replace("1.set", "")
+            newpos = str(outdata)
 
-    sdb.update({"pos":int(newpos)}, query.id == 0)
-    #TODO: update database accordingly
+        sdb.update({"pos":str(newpos)}, query.id == 1)
+
+
+    
 
     ser.write(data)
 
